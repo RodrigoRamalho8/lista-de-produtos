83e70dd302bf43dc1a992f6089f1eecd06500d08
Rodrigo Ramalho <rodrigogarcia.ramalho@gmail.com>
2025-03-27 00:01:17 -0400

    Finalizando pagina inicial index.php

--- index.php
+++ index.php
@@ -6,16 +6,27 @@
 <body>
     <?php require_once('view/components/navbar.php');?>
     <main class="main-container">
-        <div class="main-container-titulo">
-            <h1>Bem-vindo</h1>
+        <div class="main-container-conteudo">
+            <div class="main-container-conteudo-titulo">
+                <h1>Bem-vindo</h1>
+            </div>
+            <div class="main-container-conteudo-sobre-nos">
+                <div class="main-container-container-conteudo-sobre-nos-paragrafos">
+                    <p class="main-container-container-conteudo-sobre-nos-paragrafos-texto-apresentacao">
+                        Este projeto foi desenvolvido como parte de uma jornada de aprendizado e crescimento na área de programação. Criado por um desenvolvedor iniciante, ele reflete      dedicação, criatividade e o desejo de oferecer uma experiência prática e intuitiva para nossos usuários.
+                        <br>                    
+                    </p>
+                    <p class="main-container-container-conteudo-sobre-nos-paragrafos-texto-apresentacao">
+                        Nossa loja é um espaço projetado para facilitar a navegação, a busca por produtos e o processo de compra, tudo isso enquanto exploramos e aperfeiçoamos habilidades como:
+                    <br>
+                    </p>
+                    <p class="main-container-container-conteudo-sobre-nos-paragrafos-texto-apresentacao">
+                        Com este projeto, buscamos não apenas atender às suas necessidades, mas também compartilhar a paixão por tecnologia e inovação. Ele marca o início de uma jornada rumo ao desenvolvimento de soluções cada vez melhores para o mundo digital.
+                        <br>
+                        Esperamos que aproveite sua experiência em nossa loja e que este projeto inspire outros desenvolvedores iniciantes a se aventurarem em suas próprias ideias.
+                    </p>
+                </div>
+            </div>
         </div>
-        <table class="main-container-tabela">
-            <thead class="main-container-tabela-cabecalho">
-                <td>Nome</td>
-                <td>Descrição</td>
-                <td>Preço</td>
-                <td>Ações</td>
-            </thead>
-        </table>
     </main>
 </body>
