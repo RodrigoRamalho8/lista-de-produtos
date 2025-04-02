70bdc6fcd247864c5cc60b2d6d21477af6e73cae
Rodrigo Ramalho <rodrigogarcia.ramalho@gmail.com>
2025-04-01 23:46:22 -0400

    Pagina de erro e redirecionamento para o index.php

---
+++ view/pages/erro-redirect.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once "./../components/head.php";
+
+?>
+
+
+<body>    
+    <main class="main-container">
+        <div class="main-container-conteudo-erro">
+
+            <h1 class="titulo-erro-generico" >Oops!! Algum erro ocorreu em nosso site... </h1>
+            <a class="anchor-botao-erro-pagina-inicial" href="/lista-de-produtos/index.php"><button class="botao-erro-pagina-inicial">Voltar à página inicial</button></a>
+
+        </div>
+
+    </main>
+</body>
