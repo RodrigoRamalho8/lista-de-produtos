796da59ee88d3984ea07a9e48ec61c8db6422a7c
Rodrigo Ramalho <rodrigo51685776@aluno.ms.senac.br>
2025-04-02 19:51:48 -0400

    terminando o crud da classe usuario

---
+++ model/UsuarioModel.php
@@ -0,0 +1,80 @@
+<?php
+
+require_once "/lista-de-produtos/config/database.php"
+
+    class UsuarioModel {
+
+        private $conn;
+
+        private $tabela = "USUARIO";
+
+        public function __construct(){
+            $db = new Database();
+            $this->conn = $db->conectar();
+        }
+
+        public function Listar(){
+
+            $query = "SELECT * FROM $this->tabela";
+
+            $stmt=$this->conn->prepare($query);
+            $stmt->execute();
+
+            return $stmt->fetchAll();
+        }
+
+        public function BuscarPorID($id){
+
+            $query = "SELECT * FROM $this->tabela WHERE ID = $id";
+            $stmt=$this->conn->prepare($query);
+            $stmt->execute();
+            return $stmt->fetch();
+        }
+        
+        public function Adicionar($usuario){
+
+            $query = "INSERT INTO $this->tabela (CPF, NOME, DT_NASCIMENTO, EMAIL, ID_GENERO, TELEFONE) VALUES(:cpf, :nome, :dt_nascimento, :email, :id_genero, :telefone)";
+
+            $stmt=$this->conn->prepare($query);
+            $stmt->bindParam(":cpf", $usuario["CPF"]);
+            $stmt->bindParam(":nome", $usuario["NOME"]);
+            $stmt->bindParam(":dt_nascimento", $usuario["DT_NASCIMENTO"]);
+            $stmt->bindParam(":email", $usuario["EMAIL"]);
+            $stmt->bindParam(":id_genero", $usuario["ID_GENERO"]);
+            $stmt->bindParam(":telefone", $usuario["TELEFONE"]);
+
+            return $stmt->execute();
+
+        }
+
+        public function Editar($usuario){
+
+            $query = "UPDATE $this->tabela SET CPF = :cpf, NOME = :nome, DT_NASCIMENTO = :dt_nascimento, EMAIL = :email, ID_GENERO = :id_genero, TELEFONE = :telefone WHERE ID = :id";
+
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(":id", $usuario["ID"]);
+            $stmt->bindParam(":cpf", $usuario["CPF"]);
+            $stmt->bindParam(":nome", $usuario["NOME"]);
+            $stmt->bindParam(":dt_nascimento", $usuario["DT_NASCIMENTO"]);
+            $stmt->bindParam(":email", $usuario["EMAIL"]);
+            $stmt->bindParam(":id_genero", $usuario["ID_GENERO"]);
+            $stmt->bindParam(":telefone", $usuario["TELEFONE"]);
+
+            return $stmt->execute();
+
+
+
+        }
+
+        public function Excluir($usuario){
+
+            $query = "DELETE FROM $this->tabela WHERE ID = :id";
+
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(":id", $usuario["ID"]);
+            return $stmt->execute();
+
+        }
+    }
+
+?>
