f6c6bdb4b57ee9b0081a2afc5c0132431eba852c
Rodrigo Ramalho <rodrigogarcia.ramalho@gmail.com>
2025-04-01 23:17:48 -0400

    CRUD finalizado

--- view/pages/categoria_excluir.php
+++ view/pages/categoria_excluir.php
@@ -9,14 +9,18 @@ require_once './../../model/CategoriaModel.php';
 if($_SERVER['REQUEST_METHOD'] === "GET"){
     if(isset($_GET['ID'])){
         $categoriaModel = new CategoriaModel();
-        $sucesso = $categoriaModel->Excluir([
-            "ID" =>$_GET['ID']]);
-    }
-
-    if(!$sucesso){
-        return "Não foi possível excluir a categoria, devido a exitência de um produto cadastrado a esta categoria";
+        try{
+            $categoriaModel->Excluir([
+                "ID" =>$_GET['ID']]);
+        }
+        catch (Exception){
+        ?>
+            <h1 style="text-align:center;" >Ocorreu algum erro</h1>
+            <button><a href="/lista-de-produtos/index.php">Voltar à página inicial</a></button>
+        <?php
+        }
     }
 
 }
-    return header("Location: categorias.php");
+    // return header("Location: categorias.php");
 ?>
